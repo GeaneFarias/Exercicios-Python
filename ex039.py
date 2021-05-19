43195b354baf4e256bca144729c6107421b20828
Geane farias <fariassantos.geane@Gmail.com>
2021-05-18 21:21:29 -0300

    uma arrumada

--- ex039.py
+++ ex039.py
@@ -2,12 +2,15 @@ from datetime import date
 hoje = date.today().year
 from time import sleep
 ano = int(input('Qual seu ano de nascimento? '))
-alistamento = ano - 2003
-if alistamento <= -1:
-    print('Seu alistamento esta atrasado em {} anos!!'.format(alistamento))
-elif alistamento == 0:
+alist = hoje - ano
+print('Você nasceu em {}, e esta com {} anos agora em {}'.format(ano, alist, hoje))
+if alist > 18:
+    saldo = alist - 18
+    print('Seu alistamento esta atrasado em {} anos!!'.format(saldo))
+elif alist == 18:
     print('Chegou sua hora...')
     sleep(3)
-    print('HORA DE SE ALISTAR!!!')
+    print('A HORA DE SE ALISTAR!!!')
 else:
-    print('Ainda falta {} anos para seu alistamento!!'.format(alistamento))
+    saldo = 18 - alist
+    print('Ainda falta {} anos para seu alistamento!!'.format(saldo))
